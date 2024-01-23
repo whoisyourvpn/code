6d66feac0cd504d22d3b45bc19a5cd8aaf0939ce
whoisyourvpn <138953036+whoisyourvpn@users.noreply.github.com>
2024-01-23 09:34:32 -0600

    Update 5RandomNumbers.cpp

--- c++/5RandomNumbers.cpp
+++ c++/5RandomNumbers.cpp
@@ -2,13 +2,15 @@
 #include <random>
 #include <set>
 
+using namespace std;
+
 int main() {
-    std::random_device rd;  // Random device engine, usually based on /dev/random on UNIX-like systems
-    std::mt19937 rng(rd()); // Initialize Mersenne Twister pseudo-random number generator
+    random_device rd;  // Random device engine, usually based on /dev/random on UNIX-like systems
+    mt19937 rng(rd()); // Initialize Mersenne Twister pseudo-random number generator
 
-    std::uniform_int_distribution<int> uni(1, 115); // Uniformly distributed in range (1, 115)
+    uniform_int_distribution<int> uni(1, 115); // Uniformly distributed in range (1, 115)
 
-    std::set<int> random_numbers; // Set to store unique numbers
+    set<int> random_numbers; // Set to store unique numbers
 
     // Generate 5 unique random numbers
     while(random_numbers.size() < 5) {
@@ -18,9 +20,9 @@ int main() {
 
     // Print the random numbers
     for (int num : random_numbers) {
-        std::cout << num << ' ';
+        cout << num << ' ';
     }
-    std::cout << std::endl;
+    cout << endl;
 
     return 0;
 }
